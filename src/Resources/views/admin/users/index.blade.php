4e86094820cf7051d61004f5361ac47f836f3761
basuregami <basuregami@gmail.com>
2018-01-15 12:53:08 +0545

    user listing design change

--- src/Resources/views/admin/users/index.blade.php
+++ src/Resources/views/admin/users/index.blade.php
@@ -2,25 +2,43 @@
 @section('content')
     <div class="border-bottom white-bg dashboard-header">
         <div class="row">
-            <div class="col-lg-12">
+
+                <div class="col-lg-12">
+                    <a  href="{{route('users.create')}}" class="btn btn-primary pull-right"  aria-haspopup="true" aria-expanded="false">
+                        Add User</a>
                     @if( isset($users))
-                        <table class="table">
-                        <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Email</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($users as $user)
-                        <tr>
-                            <td>{{$user->name}}</td>
-                            <td>{{$user->email}}</td>
-                        </tr>
-                        </tbody>
-                        @endforeach
-                    </table>
+                        <div class="panel panel-default">
+                            <!-- Default panel contents -->
+                            <div class="panel-heading">User List</div>
+                            {{--<button class="panel-heading pull-right success">Add User</button>--}}
+
+                            <!-- Table -->
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <th>S.NO</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Action</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($users as $user)
+                                    <tr>
+                                        <td>{{$user->id}}</td>
+                                        <td>{{$user->name}}</td>
+                                        <td>{{$user->email}}</td>
+
+                                            <td>Edit | Delete</td>
+
+
+                                    </tr>
+                                </tbody>
+                                @endforeach
+                            </table>
+                        </div>
                 @endif
+
 
             </div>
         </div>
